a83c2b02e8930becbd809cf3ec841befa1542f6a
Jean-Marie TRIOMPHE <jean-marie.triomphe@external.engie.com>
2024-03-25 08:23:12 +0100

    remove tests for workshop

--- src/test/component/common/primary/app/Commandes.cy.ts
+++ src/test/component/common/primary/app/Commandes.cy.ts
@@ -1,7 +1,5 @@
 import { dataSelector } from '../../../selector.fixture';
 
-const getFirstTableRow = () => cy.get(dataSelector('commandes.table.row')).first();
-
 describe('Commandes empty', () => {
   beforeEach(() => {
     cy.intercept('GET', '/lyon-craft-2024/commandes', []).as('commandes');
@@ -12,12 +10,6 @@ describe('Commandes empty', () => {
 
   it('should not display table when commande list is empty', () => {
     cy.get(dataSelector('commandes.table')).should('not.exist');
-  });
-
-  it('should redirect to add commande form when clicking on add button', () => {
-    cy.get(dataSelector('commandes.add')).click();
-
-    cy.url().should('eq', Cypress.config().baseUrl + '/commandes/add');
   });
 });
 
@@ -35,45 +27,5 @@ describe('Commandes', () => {
   it('should display table when "commande"', () => {
     cy.get(dataSelector('commandes.table')).should('exist');
     cy.get(dataSelector('commandes.table.row')).should('have.length', 2);
-  });
-
-  it('sould display table headers', () => {
-    cy.get(dataSelector('commandes.table.header.nom')).should('contain', 'Nom');
-    cy.get(dataSelector('commandes.table.header.numero')).should('contain', 'Numéro de téléphone');
-    cy.get(dataSelector('commandes.table.header.crepes')).should('contain', 'Crêpes');
-    cy.get(dataSelector('commandes.table.header.galettes')).should('contain', 'Galettes');
-    cy.get(dataSelector('commandes.table.header.heure-de-retrait')).should('contain', 'Heure de retrait');
-  });
-
-  it('sould display client name', () => {
-    cy.get(dataSelector('commandes.table.row')).first().find(dataSelector('commandes.table.row.client.nom')).should('contain', 'nom');
-  });
-
-  it('sould display client phone number', () => {
-    getFirstTableRow().find(dataSelector('commandes.table.row.client.numero')).should('contain', '0685587326');
-  });
-
-  it('sould display all "crepes" in "commande"', () => {
-    getFirstTableRow().find(dataSelector('commandes.table.row.crepe')).should('have.length', 2);
-  });
-
-  it('sould display a "crepe" for "commande"', () => {
-    const crepesOnFirstCommande = () => getFirstTableRow().find(dataSelector('commandes.table.row.crepe'));
-
-    crepesOnFirstCommande().should('have.length', 2);
-    crepesOnFirstCommande().first().should('contain', 'price: 2 €, ingredients: sucre/beurre');
-    crepesOnFirstCommande().eq(1).should('contain', 'price: 3 €, ingredients: caramel');
-  });
-
-  it('sould display a "galette" for "commande"', () => {
-    const galettesOnFirstCommande = () => getFirstTableRow().find(dataSelector('commandes.table.row.galette'));
-
-    galettesOnFirstCommande().should('have.length', 2);
-    galettesOnFirstCommande().first().should('contain', 'price: 4 €, ingredients: oeuf');
-    galettesOnFirstCommande().eq(1).should('contain', 'price: 5 €, ingredients: fromage/oeuf');
-  });
-
-  it('sould display "heure de commande"', () => {
-    getFirstTableRow().find(dataSelector('commandes.table.row.heure-de-retrait')).should('contain', '12:30');
   });
 });
